99a3009fe8a2ab1979601c6dd579394bd8e460fa
QingWang0909 <lucifer0909@gmail.com>
2016-08-04 15:10:47 -0400

    4th commit :  emulator_manager class

--- opt/stack/neutron/neutron/plugins/ml2/odl_rest_emulator/emulator_manager.py
+++ opt/stack/neutron/neutron/plugins/ml2/odl_rest_emulator/emulator_manager.py
@@ -6,98 +6,119 @@ import sys
 import timeit
 import uuid
 
+class EmulatorManager():
+    def __init__(self):
+        self.emulator = RestEmulator()
 
+    def __get_uuid1(self):
+        return uuid.uuid1()
+
+    def __get_uuid4(self):
+        return uuid.uuid4()
+
+
+    def put_task_in_threads(self, target_func, obj):
+        try:
+            t = Thread(target=target_func, args=(obj,))
+            t.daemon = True
+            thread_pool.append(t)
+            t.start()
+
+        except KeyboardInterrupt:
+            sys.exit(1)
+
+
+    def run_task_in_threads(self):
+        for i in thread_pool:
+            i.join()
+
+
+    # Generate Subnet
+    def createSubnets(self, concurrent_subnet, network_uuid):
+        for i in range(concurrent_subnet):
+            obj = Fake_Obj_Const.Subnet
+            obj['subnet'][0]['id'] = self.__get_uuid1()
+            obj['subnet'][0]['network_id'] = network_uuid
+            self.put_task_in_threads(self.emulator.create_subnet, obj)
+
+        self.run_task_in_threads()
+
+
+    # Generate Network
+    def createNetworks(self, concurrent_client):
+        for i in range(concurrent_client):
+            obj = Fake_Obj_Const.Network
+            obj['network'][0]['id'] = self.__get_uuid1()
+            obj['network'][0]['tenant_id'] = self.__get_uuid4()
+            self.put_task_in_threads(self.emulator.create_network, obj)
+
+        self.run_task_in_threads()
+
+
+    # Delete One Network
+    def delete_One_Network(self, uuid):
+        self.emulator.del_network(uuid)
+
+
+    # Delete One Subnet
+    def delete_one_Subnet(self, uuid):
+        self.emulator.del_subnet(uuid)
+
+
+    #
+    # # Delete All Network
+    # def delete_ALL_Network(self):
+    #     pass
+    #
+    #
+    # # Delete All Subnet
+    # def delete_ALL_Subnet(self):
+    #     pass
+    #
+    # # Delete All Port
+    # def delete_ALL_Port(self):
+    #     pass
+
+
+concurrent_num = 2
 thread_pool = []
-concurrent_num = 3
-
-
-def get_uuid():
-    return uuid.uuid1()
-
-
-def put_task_in_threads(target_func, obj):
-    try:
-        t = Thread(target=target_func, args=(obj,))
-        t.daemon = True
-        thread_pool.append(t)
-        t.start()
-
-    except KeyboardInterrupt:
-        sys.exit(1)
-
-
-def run_task_in_threads():
-    for i in thread_pool:
-        i.join()
-
-
-# Generate Subnet
-def createSubnets(concurrent_num, network_uuid):
-    for i in range(concurrent_num):
-        obj = Fake_Obj_Const.Subnet
-        obj['subnet'][0]['id'] = get_uuid()
-        obj['subnet'][0]['network_id'] = network_uuid
-        put_task_in_threads(emulator.create_subnet, obj)
-
-    run_task_in_threads()
-
-
-# Generate Network
-def createNetworks(concurrent_num):
-    for i in range(concurrent_num):
-        obj = Fake_Obj_Const.Network
-        obj['network'][0]['id'] = get_uuid()
-        put_task_in_threads(emulator.create_network, obj)
-
-    run_task_in_threads()
-
-
-# Delete One Network
-def delete_One_Network(uuid):
-    emulator.del_network(uuid)
-
-
-# Delete One Subnet
-def delete_one_Subnet(uuid):
-    emulator.del_subnet(uuid)
-
-
-# Delete All Network
-def delete_ALL_Network():
-    pass
-
-
-# Delete All Subnet
-def delete_ALL_Subnet():
-    pass
-
-
 if __name__ == '__main__':
-    emulator = RestEmulator()
 
-    start = timeit.default_timer()
+    manager = EmulatorManager()
 
-    # emulator.list_security_groups()
+    while 1 :
+        start = timeit.default_timer()
 
-    # createSubnets(concurrent_num, '5f190e9e-5101-11e6-aa93-08002796ddd0')
-    # createNetworks(concurrent_num)
+        cmd = raw_input('Enter Command: ')
+        if cmd == 'q': print 'Emulator Program Closed' ; break
 
-    # delete_One_Network( 'dccc88f6-50fd-11e6-aa93-08002796ddd0' )
-    # delete_one_Subnet( '43227e42-5146-11e6-aa93-08002796ddd0' )
+        if cmd == 'c':
+            manager.createNetworks(concurrent_num)
 
-    emulator.update_one_subnet('b262ac9a-514c-11e6-aa93-08002796ddd0', Fake_Obj_Const.Subnet)
+        if cmd == 'cs':
+            pass
 
-    end = timeit.default_timer()
+        if cmd == 'd':
+            manager.delete_One_Network('4ed65f44-5a5e-11e6-aa93-08002796ddd0')
 
-    print 'Total run time is %s second' %(end - start)
-    print 'All request already sent'
+        end = timeit.default_timer()
 
-
-
-
-
-
+        # print 'Total run time is %s second' %(end - start)
+        # print 'All request already sent'
 
 
+
+
+# emulator.list_security_groups()
+
+# emulator.create_port(Fake_Obj_Const.Port)
+
+# createSubnets(concurrent_client, '5f190e9e-5101-11e6-aa93-08002796ddd0')
+
+
+# delete_One_Network( 'dccc88f6-50fd-11e6-aa93-08002796ddd0' )
+# delete_one_Subnet( '43227e42-5146-11e6-aa93-08002796ddd0' )
+
+# emulator.update_one_subnet('b262ac9a-514c-11e6-aa93-08002796ddd0', Fake_Obj_Const.Subnet)
 
 
--- opt/stack/neutron/neutron/plugins/ml2/odl_rest_emulator/mock_object_template.py
+++ opt/stack/neutron/neutron/plugins/ml2/odl_rest_emulator/mock_object_template.py
@@ -1,39 +1,6 @@
 
 class MockObjects:
 
-    # May not need this
-    DelSubnet = {
-        'subnet': [
-            {
-                'ipv6_ra_mode': None,
-                'allocation_pools': [
-                    {
-                        'start': u'10.0.2.2',
-                        'end': u'10.0.2.254'
-                    }
-                ],
-                'host_routes': [
-
-                ],
-                'ipv6_address_mode': None,
-                'cidr': u'10.0.2.0/24',
-                'id': u'7dc699ca-f705-4f40-ab22-1833d028e078',
-                'subnetpool_id': None,
-                'name': u'subnet3',
-                'enable_dhcp': True,
-                'network_id': u'f7db6cc2-ba08-47ba-8c3e-330546cd1161',
-                'tenant_id': u'487827ffd19f4da99339ec1337bfc06c',
-                'dns_nameservers': [
-
-                ],
-                'gateway_ip': u'10.0.2.1',
-                'ip_version': 4L,
-                'shared': False
-            }
-        ]
-    }
-
-
     Subnet = {
         'subnet': [
             {
@@ -74,7 +41,7 @@ class MockObjects:
             'id':u'46a18750-e165-4eef-8ef7-ebf2a7e3431f',
             'provider:segmentation_id':None,
             'router:external':False,
-            'name':u'mynetwork22',
+            'name':u'mynetwork3333',
             'admin_state_up':True,
             'tenant_id':u'487827ffd19f4da99339ec1337bfc06c',
             'provider:network_type':u'local',
@@ -85,5 +52,34 @@ class MockObjects:
     }
 
 
-
-
+    Port = {
+    "ports": [
+        {
+            "admin_state_up": True,
+            "allowed_address_pairs": [],
+            "binding:host_id": "ubuntu-amd64",
+            "binding:vif_details": [
+                {
+                    "port_filter": True
+                }
+            ],
+            "binding:vif_type": "ovs",
+            "binding:vnic_type": "normal",
+            "device_id": "reserved_dhcp_port",
+            "device_owner": "network:dhcp",
+            "extra_dhcp_opts": [],
+            "fixed_ips": [
+                {
+                    "ip_address": "10.0.2.2",
+                    "subnet_id": "e1607dfd-f71e-45e5-a1d4-045f49d17fed"
+                }
+            ],
+            "id": "e01b15fe-e1db-46aa-9c77-9bdc87b917fk",
+            "mac_address": "FA:16:3E:E9:1C:36",
+            "name": "",
+            "network_id": "0459d012-9a2c-4814-b5e0-1852c8f98bf4",
+            "security_groups": [],
+            "tenant_id": "51cc435ba80242378ae46d8eab930190"
+        }
+    ]
+    }
